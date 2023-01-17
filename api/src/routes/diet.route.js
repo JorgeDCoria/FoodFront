a4fd94f99c3e4c6ae502cb9c1c4a92289678e6f9
Jorge Coria <105801772+JorgeDCoria@users.noreply.github.com>
2023-01-17 17:29:15 -0300

    feat/ se divide repositorio en back y front por separado

--- api/src/routes/diet.route.js
+++
@@ -1,7 +0,0 @@
-const {Router} = require('express');
-const dietCtrl = require('../controllers/diet.controller');
-const router = Router();
-
-router.get("/", dietCtrl.getDiets);
-router.post("/", dietCtrl.createDiet);
-module.exports = router;
